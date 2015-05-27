af6375fe333bc8658f55381c24e540b660310fb2
Josh <jsschiff@usc.edu>
2015-05-26 21:24:59 -0700

    Fixed meanfit calculation (put inside different loop). Changed back to Hamming distance; no longer sigmoidal function. Also set g = 30, and the contribution of a match to be =0.01.

--- simulation/evonet.c
+++ simulation/evonet.c
@@ -12,7 +12,7 @@
  * N := number of generations. 
  * pop := population size. 
  */
-static const int g = 10;
+static const int g = 40;
 static const int N = 20000;
 static const int pop = 100;
 
@@ -181,7 +181,7 @@ typedef struct {
 
 int  main () {
 
-  double meanfit = 0;
+  double meanfit;
   FILE *fit; 
   fit = fopen ("fitness.dat", "w");
 
@@ -237,7 +237,7 @@ int  main () {
     for (j = 0; j < 5; j++) {
       for (k = 0; k < g; k++) {
         if (lin.generation[0].organism[org].allele[h].regulator[k] == lin.generation[0].organism[org].allele[j].protein[k]) {
-         lin.generation[0].organism[org].GRN[h][j] = (lin.generation[0].organism[org].GRN[h][j] + (10/g));
+         lin.generation[0].organism[org].GRN[h][j] = (lin.generation[0].organism[org].GRN[h][j]) + 0.5;
         /*  if (lin.generation[0].organism[org].allele[h].reg_direction == 1) {
             lin.generation[0].organism[org].GRN[h][j] = (lin.generation[0].organism[org].GRN[h][j] * (-1));
             }
@@ -339,9 +339,9 @@ for (count = 0; count < 5; count ++){
             lin.generation[gen].organism[org].allele[hap] = lin.generation[gen-1].organism[parent2].allele[hap]; 
           }
           for (int nuc = 0; nuc < g; nuc++) {
-            int mut_reg1 = rand()%10000;
+            int mut_reg1 = rand()%100000;
             int mut_reg2 = rand()%4; 
-            int mut_pro1 = rand()%10000;
+            int mut_pro1 = rand()%100000;
             int mut_pro2 = rand()%4; 
             
             if (mut_reg1 == 1 && mut_reg2 == 0) {  
@@ -373,7 +373,7 @@ for (count = 0; count < 5; count ++){
 //      printf ("%d\t%d\n", lin.generation[0].organism[0].allele[hap].protein[nuc], lin.generation[gen].organism[org].allele[hap].protein[nuc] );
           }
 //      printf ("\n"); 
-            int mut_dir = rand()%10000;
+            int mut_dir = rand()%100000;
           //  int mut_dir2 = rand()%10000;  
           if (mut_dir == 1 && lin.generation[gen].organism[org].allele[hap].reg_direction == 1) {
             lin.generation[gen].organism[org].allele[hap].reg_direction = 0; 
@@ -409,7 +409,7 @@ for (count = 0; count < 5; count ++){
             {
               if (lin.generation[gen].organism[org].allele[h].regulator[k] == lin.generation[gen].organism[org].allele[j].protein[k])
               {
-                lin.generation[gen].organism[org].GRN[h][j] = (lin.generation[gen].organism[org].GRN[h][j] + (10/g));
+                lin.generation[gen].organism[org].GRN[h][j] = (lin.generation[gen].organism[org].GRN[h][j] + 0.01);
               }
             }
 
@@ -425,7 +425,7 @@ for (count = 0; count < 5; count ++){
   {
     for (j = 0; j < 5; j++)
     {
-        lin.generation[gen].organism[org].GRN[h][j] = pow(0.5 + (exp(6 - lin.generation[gen].organism[org].GRN[h][j]))/5, -1);
+    //    lin.generation[gen].organism[org].GRN[h][j] = pow(0.5 + (exp(6 - lin.generation[gen].organism[org].GRN[h][j]))/5, -1);
       if (lin.generation[gen].organism[org].allele[h].reg_direction == 0)
       {
         lin.generation[gen].organism[org].GRN[h][j] = (lin.generation[gen].organism[org].GRN[h][j] * (-1));
@@ -441,24 +441,16 @@ for (count = 0; count < 5; count ++){
        double euc_dist = sqrt (vectors_dot_prod (lin.generation[gen].organism[org].phenotype, lin.generation[gen].organism[org].phenotype, 5) + vectors_dot_prod (optimal_phenotype, optimal_phenotype, 5) - (2 * vectors_dot_prod (optimal_phenotype, lin.generation[gen].organism[org].phenotype, 5) ) );
 //  printf ("norm = %f\n", euc_dist);
 
-       lin.generation[gen].organism[org].fitness = exp (((-1) * pow ((euc_dist)/10, 2)));
-       if ( gen % 100 == 0 && org == 0 ){
-      for (int i = 0; i < pop; i++)
-      {
-        meanfit += (lin.generation[gen].organism[i].fitness/pop);
-      
-      }
-      fprintf (fit, "%f\n,", meanfit);
-      
-    
+       lin.generation[gen].organism[org].fitness = exp (((-1) * pow ((euc_dist), 2)));
+      if ( gen % 1000 == 0 && org == 0 ){
        printf ("GENERATION %d fitness-%d = %f\n", gen, org, lin.generation[gen].organism[org].fitness);  
-         int q, r;
-          for (q = 0; q < 5; q++) {
-            for (r = 0; r < 5; r++) {
-              printf ("%f ", lin.generation[gen].organism[org].GRN[q][r]);
-            }
-            printf ("\n");
-          }
+         //int q, r;
+         // for (q = 0; q < 5; q++) {
+         //   for (r = 0; r < 5; r++) {
+         //     printf ("%f ", lin.generation[gen].organism[org].GRN[q][r]);
+         //   }
+         //   printf ("\n");
+         // }
    /*   for (hap = 0; hap < 5; hap++) {
         printf ("Gene %d\n", hap); 
         for (int i = 0; i < 10; i++) {
@@ -468,6 +460,15 @@ for (count = 0; count < 5; count ++){
           }
        } */ 
      }
+     if (gen % 100 == 0)
+     {
+         meanfit += lin.generation[gen].organism[org].fitness*0.01;
+         if (org == (pop - 1))
+         {
+           fprintf (fit, "%f\n,", meanfit);
+         }
+     }
+
     }
   }
 
